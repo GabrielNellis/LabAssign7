d87686bd089b11128b9ebfee802ef794eb441fdf
5089401 <5089401@TAG030556>
2024-11-26 12:16:33 -0700

    Add project files.

---
+++ LabAssign7.cpp
@@ -0,0 +1,16 @@
+#ifndef LabAssign7
+#define LabAssign7
+#include <iostream>
+#include <vector>
+using WORD = string;
+using WORD_LIST = set<WORD>;
+using WORD_FREQ_LIST = map<char, int>;
+using FREQ_WORD_LIST = multimap<int, WORD>;
+using COUNTER = triple<int, int, int>;
+using LONGEST = priority_queue<WORD>;
+using namespace std;
+#endif LabAssign7
+int main()
+{
+    cout << "Hello World!\n";
+}
