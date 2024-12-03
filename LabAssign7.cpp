409eced8d86e089e466e2ffdb501292fda6b73c5
5089401 <5089401@TAG030556>
2024-12-03 12:42:22 -0700

    made a few changes (like making the program in multiple files), need to make it work (and also make it in general)

--- LabAssign7.cpp
+++ LabAssign7.cpp
@@ -5,6 +5,8 @@
 #include <queue>
 #include <set>
 #include <map>
+#include <text_file.txt>
+#include <stats.cpp>
 using WORD = string; //for each word in the text file
 using WORD_LIST = set<WORD>; //for each unique word in the text file
 using WORD_FREQ_LIST = map<char, int>; //number of times each letter was used
@@ -14,7 +16,6 @@ using LONGEST = priority_queue<WORD>; //used for the histogram to help with the 
 //since histogram is going to be horizontal, like the example
 using namespace std;
 //everything needs to be sorted ALPHABETICALLY
-#endif LabAssign7
 struct DICTION {
     COUNTER stats;
     WORD_LIST words;
@@ -22,12 +23,18 @@ struct DICTION {
     FREQ_WORD_LIST freqWord;
     LONGEST longWord;
 }; 
+#endif LabAssign7
+vector<string> test{"hello", "hi", "test", "test2", "test"};
+
 WORD getNextWord(DICTION& d) {
     WORD w;
     char ch;
     bool inWord = false;
-    std::cin.get(ch);
-    while (!std::cin.eof())
+    for (char ch = 'a'; ch != 'z'; ch++) {
+        
+    }
+    std::cin.get(ch); //initializes the while loop
+    while (!std::cin.eof()) //while ???
     {
         if (isalpha(ch)) // is ch in [A-Z, a-z]
         {
@@ -35,10 +42,24 @@ WORD getNextWord(DICTION& d) {
             inWord = true;
         }
         else if (inWord) return w;
-        std::cin.get(ch);
+        std::cin.get(ch); //keeps the loop going for the length of the text file
     }
     return w;
 }
+
+void showStats(DICTION d) {
+    cout << "Dictionary Stats \n \n";
+    cout << "Number of Letters: ";
+    cout << "Number of Words  : ";
+    cout << "Number of Lines  : ";
+}
+
+DICTION buildDictionary(DICTION& d) {
+    for (auto t : test) {
+
+    }
+}
+
 int main()
 {
     DICTION d{ {0,0,0} };
---
+++ stats.cpp
@@ -0,0 +1,17 @@
+#ifndef stats.cpp
+#define stats.cpp
+#include <iostream>
+#include <LabAssign7.cpp>
+extern struct DICTION;
+#endif stats.cpp
+void drawWords(DICTION d) {
+
+}
+
+void drawLetters(DICTION d) {
+
+}
+
+void drawHistograms(DICTION d) {
+
+}
